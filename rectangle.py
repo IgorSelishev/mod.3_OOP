c8a4edc7129c98f0e0b56e30bbd5d52b5823a109
IgorSelishev <0134613@gmail.com>
2023-03-16 16:03:33 +0300

    practice OOP

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.widht = width
+        self.height = height
+    def get_widht(self):
+        return self.widht
+    def get_height(self):
+        return self.height
+    def get_area(self):
+        return self.widht * self.height
---
+++ testRectangle.py
@@ -0,0 +1,16 @@
+from rectangle import Rectangle
+
+r1 = Rectangle(10, 5)
+r2 = Rectangle(15, 10)
+
+print("r1.widht = ", r1.widht)
+print("r1.height = ", r1.height)
+print("r1.get_wight = ", r1.get_widht())
+print("r1.get_height = ", r1.get_height())
+print("r1.get_area = ", r1.get_area())
+print("_____________________________")
+print("r1.widht = ", r2.widht)
+print("r1.height = ", r2.height)
+print("r1.get_wight = ", r2.get_widht())
+print("r1.get_height = ", r2.get_height())
+print("r1.get_area = ", r2.get_area())
